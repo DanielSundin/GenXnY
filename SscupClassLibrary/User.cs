bc68f75dcfbf1c029c05701dc6dd3e807c742183
Johanna Skinnari <jskinnari@gmail.com>
2020-11-15 12:09:31 +0100

    added user class

---
+++ SscupClassLibrary/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SscupClassLibrary
+{
+    public class User
+    {
+        public stirng UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public date Birthdate { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
