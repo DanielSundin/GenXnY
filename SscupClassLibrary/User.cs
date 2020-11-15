4760df677bac0a416643cd32bb4fcf000d8f4808
Thomas Ljungdahl <ljungdahlt@gmail.com>
2020-11-15 12:29:28 +0100

    FixTypo

--- SscupClassLibrary/User.cs
+++ SscupClassLibrary/User.cs
@@ -4,11 +4,11 @@ namespace SscupClassLibrary
 {
     public class User
     {
-        public stirng UserName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public date Birthdate { get; set; }
+        public DateTime Birthdate { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
